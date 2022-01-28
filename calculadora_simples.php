331d1d9b880e31f6435a6b27618b441ea58b2576
NATHALIA APARECIDA RAMOS <21276387@CFP127ALUNO.LOCAL>
2022-01-28 16:50:20 -0300

    feito pelo professor

--- calculadora_simples.php
+++ calculadora_simples.php
@@ -1,31 +1,59 @@
 <?php
+		//declaração de varieveis
 
-		
-		$valor1 = (double) 0 ;
+		$valor1 = (double) 0;
 		$valor2 = (double) 0;
-		$resultado = (double) 0;
+		$opcao = (string) null;
+		$resultado =(double) 0;
+
+	/*O comando gettype verifica o tipo de dados na variável
+		o comando settype força a mudança de uma variável */
+
+	/* Exemplo de uma variavel que nasce do tipo inteiro e depois é
+		covertida para sitring
+	/*$nome =10;
+	echo(gettype(nome));
+
+	settype($nome, "string");
+
+	echo (gettype($nome))
+
+	strtoupper () -> coverte um texto para maiusculo
+	strtolower() -> converte um texto para minusculo
+	*/
+
+	//validação para verificar se o botao foi clicado
+	if(isset($_POST['btncalc'])) {
+		//recebe os dados do formulario
+		$valor1 = $_POST['txtn1'];
+		$valor2 = $_POST ['txtn2'];
+	
+		
+		//fazendo o tratamento da caixa vazia
+		if ($_POST ['txtn1'] == "" || $_POST['txtn2'] == "") {
+			echo ('<script> alert ("Coloque os valores"); </script>'); //fizemos isso apenas para o site dar uma alerta ao usuário
+		}else{
 			
+		if(isset($_POST['rdocalc']))
+			echo('<scrpit> alert("Favor escolher uma operação"); </script>');
+		else{
+		$opcao = strtoupper($_POST ['rdocalc']) ;
+		//Processamento do calculo das operações
+		if($opcao == 'SOMAR') 
+			$resultado = $valor1 + $valor2;
+		elseif ($opcao == 'SUBTRAIR')
+			$resultado = $valor1 - $valor2;
+		elseif ($opcao == 'MULTIPLICAR') 
+			$resultado = $valor1 * $valor2;
+		elseif ($opcao == 'DIVIDIR')
+			$resultado =$valor1 / $valor2;
+			}
+		}
+	}		
 
-		if(isset($_GET['btncalc'])){
 
-		$somar = $_GET['rdocalc'];
-		$subtrair = $_GET['rdocalc'];
-		$multiplicar= $_GET['rdocalc'];
-		$dividir = $_GET['rdocalc'];
 
-		$operacao = $_GET ['rdocalc'];
-	//tratamento para recuperar os checkbox que foram selecionados no formulario
-	if($operacao == "somar"){
-		$resultado = ($valor1 + $valor2);
-	}else if ($operacao == "subtrair"){
-		$resultado = ($valor1 - $valor2);
-	}else if($operacao == "multiplicar"){
-		$resultado = ($valor1 * $valor2);
-	}else {
-		$resultado = ($valor1 / $valor2);
-	}
 
-	}
 
 ?>
 <html>
@@ -41,11 +69,11 @@
             </div>
 
             <div id="form">
-                <form name="frmcalculadora" method="post" action="">
+                <form name="frmcalculadora" method="post" action="calculadora_simples.php">
 						Valor 1:<input type="text" name="txtn1" value="0" > <br>
 						Valor 2:<input type="text" name="txtn2" value="0" > <br>
 						<div id="container_opcoes">
-							<input type="radio" name="rdocalc" value="somar" checked>Somar <br>
+							<input type="radio" name="rdocalc" value="somar" >Somar <br>
 							<input type="radio" name="rdocalc" value="subtrair" >Subtrair <br>
 							<input type="radio" name="rdocalc" value="multiplicar" >Multiplicar <br>
 							<input type="radio" name="rdocalc" value="dividir" >Dividir <br>
@@ -54,8 +82,7 @@
 							
 						</div>	
 						<div id="resultado">
-							<?php echo ($resultado)?>
-	
+							<?php echo ($resultado);?>
 						</div>
 						
 					</form>
